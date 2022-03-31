140b69a15b0ab3d4f03c4a0ba17f0a60ffac6929
Bigyan Kumar Sandilya <bigyankumarnwd@gmail.com>
2022-03-31 18:51:55 +0530

    cart pop

--- cart/product/product.js
+++ cart/product/product.js
@@ -15,6 +15,7 @@ const box=()=>{
 //  document.querySelector("#disp").addEventListener("click", ()=>{
 
 //  })
+
 const app = (data,divA)=>{
     divA.innerHTML = ""
     data.map((ele)=>{
@@ -22,6 +23,7 @@ const app = (data,divA)=>{
         let div = document.createElement("div")
         let img = document.createElement("img")
         img.addEventListener("click",()=>{
+            ele["qnt"] = 1
             BuyNowPage(ele)
         })
         img.src = ele.src
@@ -44,6 +46,7 @@ const app = (data,divA)=>{
         let btn = document.createElement("button")
         btn.innerText = "Add To Cart"
         btn.addEventListener("click",()=>{
+            ele["qnt"] = 1
             cartPop(ele)
         })
         div.append(img,p1,h1,pricediv,btn)
@@ -144,9 +147,17 @@ rest.addEventListener("click",()=>{
     box()
     location.reload("true")
 })
+
+
+
 let dis1 = document.getElementById("cartPop")
 const cartPop = (ele)=>{
+    let Cart  = JSON.parse(localStorage.getItem("cart"))||[]
+    Cart.push(ele)
+    localStorage.setItem("cart",JSON.stringify(Cart))
+    // let Deta = JSON.parse(localStorage.getItem("cart"))
    // console.log(ele)
+   console.log(Cart)
     dis1.innerHTML = ""
     dis1.style.display = "block"
     // <div><h3><i id="disp1" class="fa-solid fa-angle-left"></i>Filter</h3></div>
@@ -170,9 +181,16 @@ const cartPop = (ele)=>{
     h3tag.innerText = "Add 2 Singal Product To Get Them @ Rs. 999"
     h3tag.style.textAlign = "center"
     div3.append(h3tag)
+    // cart Item 
+
     let div4 = document.createElement("div")
+
     let indiv = document.createElement("div")
+    indiv.setAttribute("id","cartDiv")
+    let Total = 0 
     div4.append(indiv)
+    cartAppdend(Cart,indiv,Total)
+    // end
      // <div class="btn" ><a href="#"><h4>Your Analys<i style="color: white;font-size: 15px; margin-left: 5px;" class="fa-solid fa-angle-right"></i></h4></a></div>
     let div5 = document.createElement("div")
     let inndiv1 = document.createElement("div")
@@ -207,3 +225,86 @@ const BuyNowPage = (ele)=>{
     console.log(ele)
 }
 
+
+const cartAppdend = (deta,div,Total)=>{
+    div.innerHTML =""
+    // console.log(deta,div,Total)
+    deta.map((ele,ind)=>{
+        // div1
+        let div1 =document.createElement("div")
+        div1.style.display = "flex"
+        div1.style.justifyContent = "space-between"
+        div1.setAttribute("id","div1")
+        let img = document.createElement("img")
+        img.src = ele.src
+        let h4 = document.createElement("h5")
+        h4.innerText = ele.name
+        h4.style.paddingLeft = "8px"
+        let i = document.createElement("i")
+        i.setAttribute("class","")
+        i.innerText = "*"
+        i.style.paddingRight = "10px"
+        div1.append(img,h4,i)
+        //end
+        //div2
+        let div2 = document.createElement("div")
+        div2.style.display = "flex"
+        div2.style.justifyContent = "space-between"
+        div2.setAttribute("id","div1")
+        let divsum = document.createElement("div")
+        divsum.style.display = "flex"
+        divsum.style.alignItems = "center"
+        divsum.style.gap = "10px"
+        divsum.setAttribute("id","inc")
+        let h1 = document.createElement("h3")
+        h1.addEventListener("click",()=>{
+            min(ele,ind,div,Total)
+        })
+        let i1 = document.createElement("i")
+        i1.setAttribute("class","fa-solid fa-minus")
+        h1.append(i1)
+        let h2 = document.createElement("h3")
+        h2.innerText = ele.qnt
+        let h3 =document.createElement("h3")
+        h3.addEventListener("click",()=>{
+            max(ele,ind,div,Total)
+        })
+        let i2 = document.createElement("i")
+        i2.setAttribute("class","fa-solid fa-plus")
+        h3.append(i2)
+         divsum.append(h1,h2,h3)
+        let divprice= document.createElement("div")
+        divprice.style.display = "flex"
+        divprice.style.alignItems = "center"
+        divprice.style.gap = "5px"
+        let Aprice = document.createElement("p")
+        Aprice.innerText = "Rs. "+ele.price
+        Aprice.style.textDecoration = "line-through"
+        let Dtext = document.createElement("p")
+        Dtext.innerText = "( Bundle Discount ) "
+        let Dprice = document.createElement("h4")
+        Dprice.innerText = "Rs." + (ele.price - (ele.dis*ele.price/100))
+        divprice.append(Aprice,Dtext,Dprice)
+        div2.append(divsum,divprice)
+        //end
+        div.append(div1,div2)
+    })
+}
+
+const min = (ele,ind,div,Total)=>{
+    if(ele.qnt>0){
+      let minVal =  --ele.qnt 
+      if(minVal!=0){
+        console.log(minVal)
+        ele["qnt"] = minVal
+        cartPop(ele)
+      }
+    }
+}
+const max = (ele,ind,div,Total)=>{
+    let Cart  = JSON.parse(localStorage.getItem("cart"))||[]
+    var MaxV = ele.qnt++
+    
+    Cart.push(ele)
+    console.log(Cart[ind].qnt)
+}
