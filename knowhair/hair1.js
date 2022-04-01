0043034c923793b5cf04db06138df778e966843b
APURVA-DIVAKAR <99614708+APURVA-DIVAKAR@users.noreply.github.com>
2022-04-02 00:54:42 +0530

    updated

--- knowhair/hair1.js
+++ knowhair/hair1.js
@@ -1,15 +1,64 @@
 console.log("hey");
 let obj = { 
-    frame:null
-    
+    frame:null,
+    sweat:null,
+    appetite:null,
 };
 
 
-const fun1=()=>{
-  
-  let x = document.getElementById(id).getAttribute('value'); 
-  console.log(x);
+const fun1=()=>{  
+  let x = document.getElementById("btn1").getAttribute('value'); 
+  obj.frame=x;
 }
+const fun2=()=>{  
+  let x = document.getElementById("btn2").getAttribute('value'); 
+  obj.frame=x;
+}
+const fun3=()=>{  
+  let x = document.getElementById("btn3").getAttribute('value'); 
+  obj.frame=x;
+}
+const fun4=()=>{  
+  let x = document.getElementById("frame1").getAttribute('value'); 
+  console.log(x)
+  obj.sweat=x;
+}
+const fun5=()=>{  
+  let x = document.getElementById("frame2").getAttribute('value'); 
+  console.log(x)
+  obj.sweat=x;
+}
+const fun6=()=>{  
+  let x = document.getElementById("frame3").getAttribute('value'); 
+  console.log(x)
+  obj.appetite=x;
+}
+const fun7=()=>{  
+  let x = document.getElementById("frame4").getAttribute('value'); 
+  console.log(x)
+  obj.appetite=x;
+}
+const fun8=()=>{  
+  let x = document.getElementById("frame5").getAttribute('value'); 
+  console.log(x)
+  obj.appetite=x;
+}
+const fun9=()=>{  
+  let x = document.getElementById("frame6").getAttribute('value'); 
+  console.log(x)
+  obj.appetite=x;
+}
+const next=()=>{  
+  if(obj.appetite==null||obj.sweat==null||obj.frame==null){
+    alert("Please fill all areas!!")
+  }
+  else{
+    window.location.href="hair2.html"
+  }
+}
+
+
+
 document.querySelector("#cart_page").addEventListener("click",()=>{
   console.log("Hello")
   window.location.href = "../cart.html"
--- knowhair/hair2.js
+++ knowhair/hair2.js
@@ -1,4 +1,103 @@
-document.querySelector("#cart_page").addEventListener("click",()=>{
+
+let obj = {
+    vol: null,
+    type: null,
+    htexture: null,
+    stexture: null,
+    dandruff: null,
+}
+
+const fun1=()=>{  
+    let x = document.getElementById("frame1").getAttribute('value'); 
+    console.log(x)
+    obj.vol=x;
+  }
+  const fun2=()=>{  
+    let x = document.getElementById("frame2").getAttribute('value'); 
+    console.log(x)
+    obj.vol=x;
+  }
+  const fun3=()=>{  
+    let x = document.getElementById("frame3").getAttribute('value'); 
+    console.log(x)
+    obj.vol=x;
+  }
+  const fun4=()=>{  
+    let x = document.getElementById("frame4").getAttribute('value'); 
+    console.log(x)
+    obj.type=x;
+  }
+  const fun5=()=>{  
+    let x = document.getElementById("frame5").getAttribute('value'); 
+    console.log(x)
+    obj.type=x;
+  }
+  const fun6=()=>{  
+    let x = document.getElementById("frame6").getAttribute('value'); 
+    console.log(x)
+    obj.type=x;
+  }
+  const fun7=()=>{  
+    let x = document.getElementById("frame7").getAttribute('value'); 
+    console.log(x)
+    obj.htexture=x;
+  }
+  const fun8=()=>{  
+    let x = document.getElementById("frame8").getAttribute('value'); 
+    console.log(x)
+    obj.htexture=x;
+  }
+  const fun9=()=>{  
+    let x = document.getElementById("frame9").getAttribute('value'); 
+    console.log(x)
+    obj.htexture=x;
+  }
+  const fun10=()=>{  
+    let x = document.getElementById("frame10").getAttribute('value'); 
+    console.log(x)
+    obj.stexture=x;
+  }
+  const fun11=()=>{  
+    let x = document.getElementById("frame11").getAttribute('value'); 
+    console.log(x)
+    obj.stexture=x;
+  }
+  const fun12=()=>{  
+    let x = document.getElementById("frame12").getAttribute('value'); 
+    console.log(x)
+    obj.stexture=x;
+  }
+  const fun13=()=>{  
+    let x = document.getElementById("frame13").getAttribute('value'); 
+    console.log(x)
+    obj. dandruff=x;
+  }
+  const fun14=()=>{  
+    let x = document.getElementById("frame14").getAttribute('value'); 
+    console.log(x)
+    obj.dandruff=x;
+  }
+
+
+  const pre=()=>{  
+   window.location.href="hair1.html";
+
+  }
+
+  const next=()=>{  
+    if(obj.dandruff==null||obj.stexture==null||obj.htexture==null||obj.vol==null||obj.type==null){
+        alert("Please fill all areas!!")
+      }
+      else{
+        window.location.href="hair3.html"
+      }
+
+    
+ 
+   }
+
+
+   document.querySelector("#cart_page").addEventListener("click",()=>{
     console.log("Hello")
     window.location.href = "../cart.html"
 })
@@ -6,3 +105,4 @@ document.querySelector("#login_page").addEventListener("click",()=>{
     console.log("Hello2")
     window.location.href = "../account/accounts.html"
 })
+  
