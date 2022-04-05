5e29055896f7b7d23c91dd07fb59f3cbee989e63
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-04-05 23:54:05 +0530

    Update main.js

--- reviwpage/main.js
+++ reviwpage/main.js
@@ -1,17 +1,17 @@
-import {navBar,footer,contain} from "../module/module.js"
- 
- let navbar = document.getElementById("nav")
-//console.log(navBar)
- navbar.innerHTML = navBar()
- let Footer = document.getElementById("footer")
-Footer.innerHTML = footer()
-let Container = document.getElementById("container")
-Container.innerHTML = contain()
-document.querySelector("#cart_page").addEventListener("click",()=>{
-    console.log("Hello")
-    window.location.href = "../cart.html"
-})
-document.querySelector("#login_page").addEventListener("click",()=>{
-    console.log("Hello2")
-    window.location.href = "../account/accounts.html"
-})
+import { navBar, footer, contain } from "../module/module.js";
+
+let navbar = document.getElementById("nav");
+
+navbar.innerHTML = navBar();
+let Footer = document.getElementById("footer");
+Footer.innerHTML = footer();
+let Container = document.getElementById("container");
+Container.innerHTML = contain();
+document.querySelector("#cart_page").addEventListener("click", () => {
+  console.log("Hello");
+  window.location.href = "../cart.html";
+});
+document.querySelector("#login_page").addEventListener("click", () => {
+  console.log("Hello2");
+  window.location.href = "../account/accounts.html";
+});
