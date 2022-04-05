f500cd84541639e1ded269c467487c898ea88d5d
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-04-06 00:02:40 +0530

    Update blog.js

--- blog/blog.js
+++ blog/blog.js
@@ -1,24 +1,21 @@
-import {headOrder,navBar,footer,contain} from "../module/module.js"
-let headOrd = document.getElementById("orderHead")
- 
-headOrd.innerHTML = headOrder()
-let navbar = document.getElementById("nav")
+import { headOrder, navBar, footer, contain } from "../module/module.js";
+let headOrd = document.getElementById("orderHead");
+
+headOrd.innerHTML = headOrder();
+let navbar = document.getElementById("nav");
 // console.log(navBar)
-navbar.innerHTML = navBar()
-let Footer = document.getElementById("footer")
-Footer.innerHTML = footer()
-let Container = document.getElementById("container")
-Container.innerHTML = contain()
+navbar.innerHTML = navBar();
+let Footer = document.getElementById("footer");
+Footer.innerHTML = footer();
+let Container = document.getElementById("container");
+Container.innerHTML = contain();
 
-
-function myFunction(){
-    
-}
-document.querySelector("#cart_page").addEventListener("click",()=>{
-    console.log("Hello")
-    window.location.href = "../cart.html"
-})
-document.querySelector("#login_page").addEventListener("click",()=>{
-    console.log("Hello2")
-    window.location.href = "../account/accounts.html"
-})
+function myFunction() {}
+document.querySelector("#cart_page").addEventListener("click", () => {
+  console.log("Hello");
+  window.location.href = "../cart.html";
+});
+document.querySelector("#login_page").addEventListener("click", () => {
+  console.log("Hello2");
+  window.location.href = "../account/accounts.html";
+});
